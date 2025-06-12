6c8a169f85b82cd9f184730663dd619460dd22c7
Arthur <aer47186@gmail.com>
2025-06-12 11:09:07 -0400

    fix fnasync rejects

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,19 @@
 import GetFIle from './index';
 import { ERROR_MESSAGE } from './constraints';
 import { rejects } from 'node:assert';
+// import { join } from 'node:path'
 
 
 
 (async () => {
-    const filePath = './mocks/agentes.csv';
+    const filePath = '../mocks/agentes.csv';
     const rejection = ERROR_MESSAGE.formatInvalid;
     const result = GetFIle.csvToJson(filePath);
 
-    await console.log(rejects(result, rejection))
+    // console.log(result)
+
+    return await rejects(result
+        .then((file)=> console.log(file))
+        .catch(() => rejection)
+    );
 })()
